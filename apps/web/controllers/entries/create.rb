1434b2e8e8b98376cf6644e8b6095f77ef4a9713
l3msh0 <L3msh0@gmail.com>
2021-10-15 15:16:00 +0900

    ✨ Add entries#create action

---
+++ apps/web/controllers/entries/create.rb
@@ -0,0 +1,29 @@
+module Web
+  module Controllers
+    module Entries
+      class Create
+        include Web::Action
+
+        params do
+          required(:date).filled(:str?, format?: /\A\d{4}-\d{2}-\d{2}\z/)
+          optional(:title).filled
+          required(:body).filled(:str?)
+        end
+
+        def call(params)
+          halt(422) unless params.valid?
+
+          date = params[:date]
+          title = params[:title]
+          body = params[:body]
+
+          EntryRepository.new.create(date: date, title: title, body: body)
+
+          self.body = JSON.dump({ id: 1, date: date, title: title, body: body })
+          self.headers.merge!({ "Location" => routes.entry_url(id: 1) })
+          self.status = 201
+        end
+      end
+    end
+  end
+end
