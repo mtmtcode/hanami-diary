89fe40e93b44845f877fcf91db7d70eccf8e19da
l3msh0 <L3msh0@gmail.com>
2021-10-19 12:51:52 +0900

    ✨ Add EntryRepository#list_by_desc_date

--- lib/hanami_diary/repositories/entry_repository.rb
+++ lib/hanami_diary/repositories/entry_repository.rb
@@ -1,2 +1,23 @@
 class EntryRepository < Hanami::Repository
+  def list_by_desc_date(limit: nil, year: nil, month: nil)
+    limit ||= 100
+    if (year.nil? ^ month.nil?)
+      raise ArgumentError, "year and month must be both specified or both unspecified"
+    end
+
+    if !year.nil? && !month.nil?
+      begin_of_month = Date.new(year, month, 1)
+      begin_of_next_month = (begin_of_month >> 1)
+      entries
+        .order { date.desc }
+        .limit(limit)
+        .where { (date >= begin_of_month) & (date < begin_of_next_month) }
+        .to_a
+    else
+      entries
+        .order { date.desc }
+        .limit(limit)
+        .to_a
+    end
+  end
 end
--- spec/hanami_diary/repositories/entry_repository_spec.rb
+++ spec/hanami_diary/repositories/entry_repository_spec.rb
@@ -1,3 +1,41 @@
 RSpec.describe EntryRepository, type: :repository do
-  # place your tests here
+  let(:repository) { EntryRepository.new }
+
+  before do
+    repository.clear
+  end
+
+  describe "#list_by_desc_date" do
+    it "returns a list of entries in descendent order by date" do
+      repository.create(date: Date.new(2021, 10, 9), body: "test entry1")
+      repository.create(date: Date.new(2021, 10, 10), body: "test entry2")
+
+      entries = repository.list_by_desc_date
+      expect(entries[0].date).to eq(Date.new(2021, 10, 10))
+      expect(entries[1].date).to eq(Date.new(2021, 10, 9))
+    end
+
+    it "can limit the number of entries in the result" do
+      repository.create(date: Date.new(2021, 10, 9), body: "test entry1")
+      repository.create(date: Date.new(2021, 10, 10), body: "test entry2")
+
+      entries = repository.list_by_desc_date(limit: 1)
+      expect(entries.length).to eq(1)
+    end
+
+    it "can filter entries by month" do
+      repository.create(date: Date.new(2021, 9, 30), body: "test entry1")
+      repository.create(date: Date.new(2021, 10, 1), body: "test entry2")
+      repository.create(date: Date.new(2021, 10, 31), body: "test entry3")
+      repository.create(date: Date.new(2021, 11, 1), body: "test entry4")
+
+      entries = repository.list_by_desc_date(year: 2021, month: 10)
+      expect(entries.length).to eq(2)
+      expect(entries.map(&:date).map(&:month)).to all(eq(10))
+    end
+
+    it "should be called with year and month both specified or both unspecified" do
+      expect { repository.list_by_desc_date(year: 2021) }.to raise_error
+    end
+  end
 end
