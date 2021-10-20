e6339c41ba3bd735c0629c41c38976c62b6d1ff8
l3msh0 <L3msh0@gmail.com>
2021-10-20 11:17:37 +0900

    ✨ Add pagination to EntryRepository#list_by_desc_date

--- lib/hanami_diary/repositories/entry_repository.rb
+++ lib/hanami_diary/repositories/entry_repository.rb
@@ -1,9 +1,10 @@
 class EntryRepository < Hanami::Repository
-  def list_by_desc_date(limit: nil, year: nil, month: nil)
-    limit ||= 100
+  def list_by_desc_date(limit:, page: 1, year: nil, month: nil)
     if (year.nil? ^ month.nil?)
       raise ArgumentError, "year and month must be both specified or both unspecified"
     end
+
+    offset = limit * (page - 1)
 
     if !year.nil? && !month.nil?
       begin_of_month = Date.new(year, month, 1)
@@ -11,12 +12,14 @@ class EntryRepository < Hanami::Repository
       entries
         .order { date.desc }
         .limit(limit)
+        .offset(offset)
         .where { (date >= begin_of_month) & (date < begin_of_next_month) }
         .to_a
     else
       entries
         .order { date.desc }
         .limit(limit)
+        .offset(offset)
         .to_a
     end
   end
--- spec/hanami_diary/repositories/entry_repository_spec.rb
+++ spec/hanami_diary/repositories/entry_repository_spec.rb
@@ -10,32 +10,46 @@ RSpec.describe EntryRepository, type: :repository do
       repository.create(date: Date.new(2021, 10, 9), body: "test entry1")
       repository.create(date: Date.new(2021, 10, 10), body: "test entry2")
 
-      entries = repository.list_by_desc_date
+      entries = repository.list_by_desc_date(limit: 10)
+
       expect(entries[0].date).to eq(Date.new(2021, 10, 10))
       expect(entries[1].date).to eq(Date.new(2021, 10, 9))
     end
 
-    it "can limit the number of entries in the result" do
+    it "limits the number of result" do
       repository.create(date: Date.new(2021, 10, 9), body: "test entry1")
       repository.create(date: Date.new(2021, 10, 10), body: "test entry2")
 
       entries = repository.list_by_desc_date(limit: 1)
+
       expect(entries.length).to eq(1)
     end
 
-    it "can filter entries by month" do
+    it "returns results in the specified page" do
+      repository.create(date: Date.new(2021, 10, 9), body: "test entry1")
+      repository.create(date: Date.new(2021, 10, 10), body: "test entry2")
+
+      entries = repository.list_by_desc_date(limit: 1, page: 2)
+
+      expect(entries.length).to eq(1)
+      expect(entries[0].date).to eq(Date.new(2021, 10, 9))
+    end
+
+    it "filters entries by month" do
       repository.create(date: Date.new(2021, 9, 30), body: "test entry1")
       repository.create(date: Date.new(2021, 10, 1), body: "test entry2")
       repository.create(date: Date.new(2021, 10, 31), body: "test entry3")
       repository.create(date: Date.new(2021, 11, 1), body: "test entry4")
 
-      entries = repository.list_by_desc_date(year: 2021, month: 10)
+      entries = repository.list_by_desc_date(limit: 100, year: 2021, month: 10)
+
       expect(entries.length).to eq(2)
       expect(entries.map(&:date).map(&:month)).to all(eq(10))
     end
 
     it "should be called with year and month both specified or both unspecified" do
-      expect { repository.list_by_desc_date(year: 2021) }.to raise_error
+      expect { repository.list_by_desc_date(year: 2021) }.to raise_error(ArgumentError)
+      expect { repository.list_by_desc_date(month: 10) }.to raise_error(ArgumentError)
     end
   end
 end
